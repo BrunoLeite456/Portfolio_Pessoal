9f73a5a7f7d81b71b790d80fdb7cfdbf1b719bec
BRUNO RICARDO LEITE <bruno.leite1@alunos.sc.senac.br>
2026-07-28 20:38:21 -0300

    portfolio

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 const cards = document.querySelectorAll(".card, .projeto");
 
-const observer = new IntersectionObserver((entries)=>{
+const observer = new IntersectionObserver((entries) => {
 
-entries.forEach(entry=>{
+    entries.forEach(entry => {
 
-if(entry.isIntersecting){
+        if (entry.isIntersecting) {
 
-entry.target.classList.add("show");
+            entry.target.classList.add("show");
 
+        }
+
+    });
+
+});
+
+cards.forEach(card => {
+
+    card.classList.add("hidden");
+
+    observer.observe(card);
+
+});
+
+function abrirPhoenix() {
+    document.getElementById("phoenixModal").style.display = "flex";
 }
 
-});
+function fecharPhoenix() {
+    document.getElementById("phoenixModal").style.display = "none";
+}
 
-});
+window.onclick = function (event) {
+    const modal = document.getElementById("phoenixModal");
 
-cards.forEach(card=>{
-
-card.classList.add("hidden");
-
-observer.observe(card);
-
-});
-
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
